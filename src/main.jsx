9c388cd56a6af06af6c3a3f12bbfc1d073d39368
rao_ankit_9001 <raoankityadav9001@gmail.com>
2024-07-15 15:55:43 +0530

    add react hot tost

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import { Toaster } from 'react-hot-toast'
+import store from './Redux/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
   <BrowserRouter>
     <App />
+    <Toaster />
   </BrowserRouter>
+  </Provider>
 
 
   
